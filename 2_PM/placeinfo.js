8671c0a3ebc0062c9970fdbe156b5fc40a2c6e3c
David Chen <dychen@college.harvard.edu>
2019-01-08 16:31:04 -0500

    Add actual functionality

--- 2_PM/placeinfo.js
+++ 2_PM/placeinfo.js
@@ -2,3 +2,20 @@
 / returns the following string, formatted with the appropriate values:
 
 'INPUTTED_PLACE is located at coordinates RETURNED_COORDINATES, at the address RETURNED_ADDRESS.' */
+const googleMapsClient = require("./google_api.js");
+
+googleMapsClient.geocode({
+    address: process.argv[2]
+  }, function(err, response) {
+    if (!err) {
+      googleMapsClient.place({
+          placeid: response.json.results[0].place_id, 
+      }, function(err, response) {
+          if (!err) {
+              const coord = response.json.result.geometry.location;
+              const address = response.json.result.formatted_address;
+              console.log(`${process.argv[2]} is located at coordinates ${coord.lat}, ${coord.lng} at the address ${address}.`);
+          }
+      });
+    }
+  });
