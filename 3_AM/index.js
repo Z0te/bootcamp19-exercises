10680ee8cd04112aead3f5eeaf20658496f59147
David Chen <dychen@college.harvard.edu>
2019-01-09 16:59:27 -0500

    Create HTML pages and implement behavior

--- 3_AM/index.js
+++ 3_AM/index.js
@@ -1,12 +1,27 @@
 const express = require("express");
+const path = require('path');
 const app = express();
-
-app.use(express.static(__dirname + "/View"));
-app.use(express.static(__dirname + "/Script"));
+app.use(express.static('public'))
 
 app.get("/", (req, res) => {
-  res.sendFile("index.html");
+  res.sendFile(path.join(__dirname + "/index.html"));
 });
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname + "/about.html"));
+})
+
+app.get("/prize", (req, res) => {
+  res.sendFile(path.join(__dirname + "/prize.html"));
+})
+
+app.get("/page4", (req, res) => {
+  res.sendFile(path.join(__dirname + "/page4.html"));
+})
+
+app.get("/winner", (req, res) => {
+  res.sendFile(path.join(__dirname + "/winner.html"));
+})
 
 app.listen(3000);
 console.log("Running at port 3000");
